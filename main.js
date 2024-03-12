4a9bc3fb633fa4b0368288b63c94b15b0c7f181b
Taj Smith <TajSmith@Deloitte.com>
2024-03-12 15:53:29 -0400

    Fixed media and main

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // Import classes here to console.log and debug
+const Media = require ('./classes/Media.js');
+const Music = require ('./classes/Music.js');
+const Movie = require('./classes/Movie.js');
+const Book = require('./classes/Book.js');
